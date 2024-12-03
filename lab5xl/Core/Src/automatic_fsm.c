3c627f879e00e118798abf84d45f2055a64867f5
LAPTOP-PFJ3FL1R\ASUS <son.nguyenha288@hcmut.edu.vn>
2024-12-03 16:29:31 +0700

    Init Lab5

--- lab5xl/Core/Src/automatic_fsm.c
+++ lab5xl/Core/Src/automatic_fsm.c
@@ -5,7 +5,7 @@ extern ADC_HandleTypeDef hadc1;
 extern UART_HandleTypeDef huart2;
 
 int status1 = INIT;
-int ADC_value = 0;
+float ADC_value = 0;
 
 uint8_t buffer_byte;
 uint8_t buffer[MAX_BUFFER_SIZE];
@@ -20,7 +20,7 @@ int isCmdEqualToRST(uint8_t str[]) {
 	if (str[0] == 'R' && str[1] == 'S' && str[2] == 'T')
 		return 1;
 	else
-		return 0;
+		return 	0;
 }
 
 int isCmdEqualToOK(uint8_t str[]) {
@@ -61,21 +61,41 @@ void cmd_parser_fsm() {
 }
 
 void uart_comms_fsm() {
-	char str[50];
-	switch (cmd_flag) {
-	case RST:
-		if (timer1_flag == 1) {
-			ADC_value = HAL_ADC_GetValue(&hadc1);
-			HAL_UART_Transmit(&huart2, (void*) str,
-					sprintf(str, "!ADC=%d#\r\n", ADC_value), 500);
-			setTimer1(100);
-		}
-		break;
-	case OK:
-		ADC_value = -1;
-		cmd_flag = INIT;
-		break;
-	default:
-		break;
-	}
+    static char last_packet[50];  // Lưu gói tin cuối cùng
+    static float adc_stored_value = 0; // Lưu giá trị ADC trước đó
+    static int adc_locked = 0; // Cờ khóa giá trị ADC
+    char str[50];
+
+    switch (cmd_flag) {
+    case RST:
+        if (!adc_locked) {
+            // Đọc giá trị ADC khi nhận lệnh RST lần đầu
+            adc_stored_value = HAL_ADC_GetValue(&hadc1) * 5.0 / 4096;
+            adc_locked = 1;  // Khóa giá trị ADC
+        }
+
+        if (timer1_flag == 1) {
+            int int_part = (int)adc_stored_value;
+            int dec_part = (int)((adc_stored_value - int_part) * 100);
+            sprintf(last_packet, "!ADC=%d.%02d#\r\n", int_part, dec_part);
+            HAL_UART_Transmit(&huart2, (uint8_t*)last_packet, strlen(last_packet), 500);
+            setTimer1(300);  // 3 giây
+            timer1_flag = 0;
+        }
+        break;
+
+    case OK:
+        adc_locked = 0;  // Mở khóa cho phép cập nhật giá trị ADC mới
+        HAL_UART_Transmit(&huart2, (uint8_t*)"STOP#\r\n", strlen("STOP#\r\n"), 500);
+        cmd_flag = INIT;  // Reset trạng thái FSM
+        break;
+
+    default:
+        break;
+    }
 }
+
+
+
+
+
